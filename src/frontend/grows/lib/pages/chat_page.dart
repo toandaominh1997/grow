be9c7d29209a02d749bb0dde972fdb0841f7e1fe
tonne <toandaominh1997@gmail.com>
2021-06-12 11:28:04 +0700

    Fix chat page

--- src/frontend/grows/lib/pages/chat_page.dart
+++ src/frontend/grows/lib/pages/chat_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:grows/json/chat_json.dart';
 import 'package:grows/theme/colors.dart';
-import 'package:grows/pages/chat_detail_page.dart';
+// import 'package:grows/pages/chat_detail_page.dart';
 
 class ChatPage extends StatefulWidget {
   @override
@@ -15,11 +15,11 @@ class _ChatPageState extends State<ChatPage> {
     return Scaffold(
       backgroundColor: bgColor,
       appBar: getAppBar(),
-      body: getBody(),
+      body: getBodyne(),
     );
   }
 
-  Widget getAppBar() {
+  PreferredSizeWidget getAppBar() {
     return AppBar(
       backgroundColor: bgColor,
       leading: IconButton(
@@ -42,7 +42,7 @@ class _ChatPageState extends State<ChatPage> {
     );
   }
 
-  Widget getBody() {
+  Widget getBodyne() {
     var size = MediaQuery.of(context).size;
     return ListView(
       children: [
@@ -51,14 +51,10 @@ class _ChatPageState extends State<ChatPage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                "Chats",
-                style: TextStyle(
-                    fontSize: 23, color: white, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 15,
-              ),
+              Text("charts",
+                  style: TextStyle(
+                      fontSize: 23, color: white, fontWeight: FontWeight.bold)),
+              SizedBox(height: 15),
               Container(
                 height: 38,
                 decoration: BoxDecoration(
@@ -76,17 +72,15 @@ class _ChatPageState extends State<ChatPage> {
                           color: white.withOpacity(0.3), fontSize: 17)),
                 ),
               ),
-              SizedBox(
-                height: 25,
-              ),
+              SizedBox(height: 25),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
-                    "Broadcast Lists",
+                    "Boradcast Lists",
                     style: TextStyle(
-                        fontSize: 16,
                         color: primary,
+                        fontSize: 16,
                         fontWeight: FontWeight.w500),
                   ),
                   Text(
@@ -95,9 +89,9 @@ class _ChatPageState extends State<ChatPage> {
                         fontSize: 16,
                         color: primary,
                         fontWeight: FontWeight.w500),
-                  )
+                  ),
                 ],
-              )
+              ),
             ],
           ),
         ),
@@ -105,96 +99,221 @@ class _ChatPageState extends State<ChatPage> {
           padding: const EdgeInsets.only(left: 5, top: 5, right: 5),
           child: Divider(color: white.withOpacity(0.3)),
         ),
-        SizedBox(
-          height: 10,
-        ),
+        SizedBox(height: 10),
         Column(
             children: List.generate(chat_data.length, (index) {
           return GestureDetector(
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (_) => ChatDetailPage(
-                            name: chat_data[index]['name'],
-                            img: chat_data[index]['img'],
-                          )));
-            },
-            child: Padding(
-              padding: const EdgeInsets.only(left: 15, right: 15, bottom: 5),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    width: 60,
-                    height: 60,
-                    decoration: BoxDecoration(
+              onTap: () {},
+              child: Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15, bottom: 15),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
                         shape: BoxShape.circle,
                         image: DecorationImage(
                             image: NetworkImage(chat_data[index]['img']),
-                            fit: BoxFit.cover)),
-                  ),
-                  SizedBox(
-                    width: 15,
-                  ),
-                  Expanded(
-                    child: Container(
-                      height: 85,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Container(
-                                    width: (size.width - 40) * 0.6,
-                                    child: Text(chat_data[index]['name'],
-                                        style: TextStyle(
-                                            fontSize: 16,
-                                            color: white,
-                                            fontWeight: FontWeight.w600)),
-                                  ),
-                                  Expanded(
-                                    child: Text(
-                                      chat_data[index]['date'],
-                                      style: TextStyle(
-                                          fontSize: 14,
-                                          color: white.withOpacity(0.4)),
-                                    ),
-                                  )
-                                ],
-                              ),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Container(
-                                width: (size.width - 40) * 1,
-                                child: Text(
-                                  chat_data[index]['text'],
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      height: 1.3,
-                                      color: white.withOpacity(0.4)),
-                                ),
-                              )
-                            ],
-                          ),
-                          Divider(
-                            color: white.withOpacity(0.3),
-                          )
-                        ],
+                            fit: BoxFit.cover),
                       ),
                     ),
-                  )
-                ],
-              ),
-            ),
-          );
+                    SizedBox(width: 15),
+                    Expanded(
+                      child: Container(
+                        height: 85,
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(children: [
+                                Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Container(
+                                          width: (size.width - 40) * 0.6,
+                                          child: Text(chat_data[index]['name'],
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color: white.withOpacity(0.4),
+                                              ))),
+                                    ]),
+                                SizedBox(height: 5),
+                                Container(
+                                    width: (size.width - 40) * 1,
+                                    child: Text(
+                                      chat_data[index]['text'],
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          height: 1.3,
+                                          color: white.withOpacity(0.4)),
+                                    )),
+                              ]),
+                              Divider(color: white.withOpacity(0.3)),
+                            ]),
+                      ),
+                    ),
+                  ],
+                ),
+              ));
         }))
       ],
     );
   }
+//   Widget getBody() {
+//     var size = MediaQuery.of(context).size;
+//     return ListView(
+//       children: [
+//         Padding(
+//           padding: EdgeInsets.only(top: 15, right: 15, left: 15),
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               Text(
+//                 "Chats",
+//                 style: TextStyle(
+//                     fontSize: 23, color: white, fontWeight: FontWeight.bold),
+//               ),
+//               SizedBox(
+//                 height: 15,
+//               ),
+//               Container(
+//                 height: 38,
+//                 decoration: BoxDecoration(
+//                     color: textfieldColor,
+//                     borderRadius: BorderRadius.circular(10)),
+//                 child: TextField(
+//                   style: TextStyle(color: white),
+//                   cursorColor: primary,
+//                   decoration: InputDecoration(
+//                       prefixIcon:
+//                           Icon(LineIcons.search, color: white.withOpacity(0.3)),
+//                       border: InputBorder.none,
+//                       hintText: "Search",
+//                       hintStyle: TextStyle(
+//                           color: white.withOpacity(0.3), fontSize: 17)),
+//                 ),
+//               ),
+//               SizedBox(
+//                 height: 25,
+//               ),
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                 children: [
+//                   Text(
+//                     "Broadcast Lists",
+//                     style: TextStyle(
+//                         fontSize: 16,
+//                         color: primary,
+//                         fontWeight: FontWeight.w500),
+//                   ),
+//                   Text(
+//                     "New Group",
+//                     style: TextStyle(
+//                         fontSize: 16,
+//                         color: primary,
+//                         fontWeight: FontWeight.w500),
+//                   )
+//                 ],
+//               )
+//             ],
+//           ),
+//         ),
+//         Padding(
+//           padding: const EdgeInsets.only(left: 5, top: 5, right: 5),
+//           child: Divider(color: white.withOpacity(0.3)),
+//         ),
+//         SizedBox(
+//           height: 10,
+//         ),
+//         Column(
+//             children: List.generate(chat_data.length, (index) {
+//           return GestureDetector(
+//             onTap: () {
+//               Navigator.push(
+//                   context,
+//                   MaterialPageRoute(
+//                       builder: (_) => ChatDetailPage(
+//                             name: chat_data[index]['name'],
+//                             img: chat_data[index]['img'],
+//                           )));
+//             },
+//             child: Padding(
+//               padding: const EdgeInsets.only(left: 15, right: 15, bottom: 5),
+//               child: Row(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   Container(
+//                     width: 60,
+//                     height: 60,
+//                     decoration: BoxDecoration(
+//                         shape: BoxShape.circle,
+//                         image: DecorationImage(
+//                             image: NetworkImage(chat_data[index]['img']),
+//                             fit: BoxFit.cover)),
+//                   ),
+//                   SizedBox(
+//                     width: 15,
+//                   ),
+//                   Expanded(
+//                     child: Container(
+//                       height: 85,
+//                       child: Column(
+//                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                         children: [
+//                           Column(
+//                             children: [
+//                               Row(
+//                                 mainAxisAlignment:
+//                                     MainAxisAlignment.spaceBetween,
+//                                 children: [
+//                                   Container(
+//                                     width: (size.width - 40) * 0.6,
+//                                     child: Text(chat_data[index]['name'],
+//                                         style: TextStyle(
+//                                             fontSize: 16,
+//                                             color: white,
+//                                             fontWeight: FontWeight.w600)),
+//                                   ),
+//                                   Expanded(
+//                                     child: Text(
+//                                       chat_data[index]['date'],
+//                                       style: TextStyle(
+//                                           fontSize: 14,
+//                                           color: white.withOpacity(0.4)),
+//                                     ),
+//                                   )
+//                                 ],
+//                               ),
+//                               SizedBox(
+//                                 height: 5,
+//                               ),
+//                               Container(
+//                                 width: (size.width - 40) * 1,
+//                                 child: Text(
+//                                   chat_data[index]['text'],
+//                                   style: TextStyle(
+//                                       fontSize: 15,
+//                                       height: 1.3,
+//                                       color: white.withOpacity(0.4)),
+//                                 ),
+//                               )
+//                             ],
+//                           ),
+//                           Divider(
+//                             color: white.withOpacity(0.3),
+//                           )
+//                         ],
+//                       ),
+//                     ),
+//                   )
+//                 ],
+//               ),
+//             ),
+//           );
+//         }))
+//       ],
+//     );
+//   }
 }
--- src/frontend/grows/lib/pages/status_page.dart
+++ src/frontend/grows/lib/pages/status_page.dart
@@ -19,7 +19,7 @@ class _StatusPageState extends State<StatusPage> {
     );
   }
 
-  Widget getAppBar() {
+  PreferredSizeWidget getAppBar() {
     return AppBar(
       backgroundColor: bgColor,
       title: Row(
