ff26d30c38879d8663c0337f60ed8406b5152f48
tonne <toandaominh1997@gmail.com>
2021-09-27 20:15:40 +0700

    Complete postgres

--- src/services/postgres/test.py
+++ src/services/postgres/test.py
@@ -1,14 +1,4 @@
 import psycopg2
+conn = psycopg2.connect(database = "postgres", user = "user", password = "pass", host = "0.0.0.0", port = "5432")
 
-conn = psycopg2.connect(database = "database", user = "postgres", password = "mysecretpassword", host = "0.0.0.0", port = "5432")
 
-cur = conn.cursor()
-cur.execute('''CREATE TABLE COMPANY
-      (ID INT PRIMARY KEY     NOT NULL,
-      NAME           TEXT    NOT NULL,
-      AGE            INT     NOT NULL,
-      ADDRESS        CHAR(50),
-      SALARY         REAL);''')
-
-conn.commit()
-conn.close()
