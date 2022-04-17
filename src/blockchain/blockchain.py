22b81326dab6bbbec0c7644399ad8531c89d62c2
toandaominh1997 <toandaominh1997@gmail.com>
2022-04-18 01:15:47 +0700

    add jenkins rabbitmq and traefik

---
+++ src/blockchain/blockchain.py
@@ -0,0 +1,68 @@
+import hashlib
+import json
+from time import time
+from uuid import uuid4
+from urllib.parse import urlparse
+
+class BlockChain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+        self.nodes = set()
+        # create the genesis block
+        self.new_block(previous_hash=1, proof=100)
+
+    @staticmethod
+    def hash(block):
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index': len(self.chain)+1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    def new_transaction(self, sender, recipient, amount):
+        self.current_transactions.append({
+            "sender":sender,
+            "recient":recipient,
+            "data":amount,
+        })
+        return int(self.last_block['index'])+1
+
+    def proof_of_work(self, last_proof):
+        # simple proof of work algorithm
+        # find a number p' such as hash(pp') containing leading 4 zeros where p is the previous p'
+        # p is the previous proof and p' is the new proof
+        proof = 0
+        while self.validate_proof(last_proof, proof) is False:
+            proof += 1
+        return proof
+
+    @staticmethod
+    def validate_proof(last_proof, proof):
+        # validates the proof: does hash(last_proof, proof) contain 4 leading zeroes?
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+    def register_node(self, address):
+        # add a new node to the list of nodes
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def full_chain(self):
+        # xxx returns the full chain and a number of blocks
+        pass
