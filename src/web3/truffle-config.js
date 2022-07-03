7031d56b6c66e91a2890a56afb32bbe33f253e5c
toandaominh1997 <toandaominh1997@gmail.com>
2022-07-03 19:27:14 +0700

    add ganache

--- src/web3/truffle-config.js
+++ src/web3/truffle-config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    }
+  }
   // Uncommenting the defaults below
   // provides for an easier quick-start with Ganache.
   // You can also follow this format for other networks.
