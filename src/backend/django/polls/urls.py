f1c48887ca9b7462304df99c7012fac33d29d61d
toandaominh1997 <toandaominh1997@gmail.com>
2022-07-10 05:03:33 +0700

    Add rest_framework

--- src/backend/django/polls/urls.py
+++ src/backend/django/polls/urls.py
@@ -1,13 +1,20 @@
-from django.urls import path 
-
+from django.urls import path, include
+from rest_framework import routers
 from . import views 
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 app_name = 'polls'
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
+    path('index', views.index, name = 'index'),
     path('<int:question_id>/', views.detail, name = 'detail'),
     path('<int:question_id>/results/', views.results, name = 'results'),
     path('<int:question_id>/vote/', views.vote, name = 'vote'),
-    path('tiny_url', views.show_tinyurl, name = 'tiny_url')
+    path('tiny_url', views.show_tinyurl, name = 'tiny_url'),
+
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
