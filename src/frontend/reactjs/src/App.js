f32b8d89efb665a0ab7ea5698b0ff2f53f9fe0a6
toandaominh1997 <toandaominh1997@gmail.com>
2022-07-22 02:20:16 +0700

    Add navbar

--- src/frontend/reactjs/src/App.js
+++ src/frontend/reactjs/src/App.js
@@ -1,34 +1,50 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Test from "./components/Test";
-import Home from "./components/Home";
-import SignIn from "./components/SignIn";
-import Search from "./components/Search";
+import Navbar from "./components/Navbar";
+import Test from "./pages/Home";
+import Home from "./pages/Home";
+import SignIn from "./pages/Search";
+import Search from "./pages/Search";
 import TinyURL from "./components/TinyURL";
-import Blog from "./components/Blog";
+import Blog from "./pages/Blog";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import { Layout } from "antd";
+
+const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
   return (
     <div className="container">
-      <Router>
-        <div className="App">
-          <Routes>
-            <Route exact path="/" element={<Home />}></Route>
-            <Route path="/signin" element={<SignIn />}></Route>
-            <Route path="/search" element={<Search />}></Route>
-            <Route path="/test" element={<Test />}></Route>
-            <Route path="/tinyurl" element={<TinyURL />}></Route>
-            <Route path="/blog" element={<Blog />}></Route>
-          </Routes>
-        </div>
-      </Router>
-	  <footer>
-		<small>
-		made by <a href="https://twitter.com/krzysu">Henry</a>, source code available on <a href="https://github.com/krzysu/reactjs-shopping-cart">github</a>
-		</small>
-	  </footer>
+      <Layout>
+        <Router>
+          <Sider>
+            <Navbar />
+          </Sider>
+          <Layout>
+            <Header>Header</Header>
+            <Content>
+              <Routes>
+                <Route extra path="/" element={<Home />}></Route>
+                <Route path="/signin" element={<SignIn />}></Route>
+                <Route path="/search" element={<Search />}></Route>
+                <Route path="/test" element={<Test />}></Route>
+                <Route path="/tinyurl" element={<TinyURL />}></Route>
+                <Route path="/blog" element={<Blog />}></Route>
+              </Routes>
+            </Content>
+            <Footer>
+              <small>
+                made by <a href="https://twitter.com/krzysu">Henry</a>, source
+                code available on{" "}
+                <a href="https://github.com/krzysu/reactjs-shopping-cart">
+                  github
+                </a>
+              </small>
+            </Footer>
+          </Layout>
+        </Router>
+      </Layout>
     </div>
   );
 }
